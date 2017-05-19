a5aba11634b7359d602165ff83af5de64b302936
yoyo <yoyo@yoyotekiMacBook-Air.local>
2017-05-19 14:21:26 +0800

    First commit for hello test

---
+++ build/CMakeFiles/testGit.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testGit.cpp.dir/testGit.cpp.o"
+  "testGit.cpp.pdb"
+  "testGit.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testGit.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
